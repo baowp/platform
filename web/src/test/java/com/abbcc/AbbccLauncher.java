4978a398bc2ad1f24f4f3a6bb611e0fb17d376a9
baowp <122515909@qq.com>
2014-04-28 18:38:16 +0800

    web

---
+++ web/src/test/java/com/abbcc/AbbccLauncher.java
@@ -0,0 +1,13 @@
+package com.abbcc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: baowp
+ * Date: 14-4-28
+ * Time: 下午3:47
+ */
+public class AbbccLauncher {
+    public static void main(String[] args){
+        org.eclipse.jetty.diy.launch.Main.main(args);
+    }
+}
